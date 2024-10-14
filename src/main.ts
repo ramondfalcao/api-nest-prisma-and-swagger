e0ef2810f4b4a30a71441ed837c68e26a777dc4b
ramondfalcao <ramondfalcao@gmail.com>
2024-10-14 10:52:55 -0300

    Adding validation pipe with class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
-    .setTitle('Median')
+    .setTitle('BLOG API WITH NEST.JS AND PRISMA')
     .setDescription('The Median API description')
     .setVersion('0.1')
     .build();
