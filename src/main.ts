ebc1686fbbb4df314609ed3e2b31ff191686975a
ramondfalcao <ramondfalcao@gmail.com>
2024-10-14 11:15:10 -0300

    Stripped client request of unnecessary properties and integrated ParseIntPipe to parse a string path variable and convert it to a number

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
     .setTitle('BLOG API WITH NEST.JS AND PRISMA')
